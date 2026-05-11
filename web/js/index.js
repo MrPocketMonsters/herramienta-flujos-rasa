681b4a38be3735b182173fedaf0c5ad5e9bedab2
MrPocketMonsters <ndsabogalv@gmail.com>
2026-05-11 18:12:32 -0500

    feat: Configuración formulario estados

--- web/js/index.js
+++ web/js/index.js
@@ -6,12 +6,29 @@
     }
   }
 
+  function setSelectTestFormularioOptions() {
+    var select = document.querySelector('#select-test-formulario');
+    if (!select) return;
+
+    var configFiles = [
+      "02_estados.formulario.json",
+    ];
+
+    configFiles.forEach(file => {
+      const option = document.createElement('option');
+      option.value = file;
+      option.textContent = file;
+      select.appendChild(option);
+    });
+  }
+
   setYear();
+  setSelectTestFormularioOptions();
 })();
 
 function goToFormulario() {
-  var textInput = document.querySelector('#text-test-formulario');
-  if (textInput) {
-    window.location.href = `html/formulario.html?${encodeURIComponent(textInput.value)}`;
+  var value = document.querySelector('#select-test-formulario').value;
+  if (value) {
+    window.location.href = `html/formulario.html?config=${encodeURIComponent(value)}`;
   }
 }
